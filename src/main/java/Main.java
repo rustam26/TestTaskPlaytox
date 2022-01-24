fe805d1c414959d0622cb8cea48c2d80f4c1aa34
rustam26 <rustam.ru26@mail.ru>
2022-01-24 23:01:32 +0300

    Comnitis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,29 +12,34 @@ public class Main {
         Account account4 = new Account(String.valueOf(random.nextInt(10)), 10000);
 
 
-        MyThread thread1 = new MyThread(account2, account1, 1);
-        MyThread thread2 = new MyThread(account1, account2, 1000);
-        MyThread thread3 = new MyThread(account1, account2, 1000);
-        MyThread thread4 = new MyThread(account2, account4, 1000);
-        MyThread thread5 = new MyThread(account2, account4, 1000);
-        MyThread thread6 = new MyThread(account3, account2, 1000);
+        MyThread thread1 = new MyThread(account2, account1, random.nextInt(11000));
+        MyThread thread2 = new MyThread(account1, account2, random.nextInt(11000));
+        MyThread thread3 = new MyThread(account1, account2, random.nextInt(11000));
+        MyThread thread4 = new MyThread(account2, account4, random.nextInt(11000));
+        MyThread thread5 = new MyThread(account2, account4, random.nextInt(11000));
+        MyThread thread6 = new MyThread(account3, account2, random.nextInt(11000));
 
 
         thread1.start();
         thread2.start();
         thread3.start();
         thread4.start();
-
+        thread5.start();
+        thread6.start();
 
         thread1.join();
         thread2.join();
         thread3.join();
+        thread4.join();
+        thread5.join();
+        thread6.join();
 
 
         System.out.println(account1.getMoney());
         System.out.println(account2.getMoney());
         System.out.println(account3.getMoney());
         System.out.println(account4.getMoney());
+        System.out.println(account1.getMoney()+account2.getMoney()+account3.getMoney()+account4.getMoney());
     }
 
 }
@@ -55,35 +60,38 @@ class MyThread extends Thread {
     @Override
     public void run() {
 
-
-        while (count <= 30 ) {
+        while (true) {
             try {
-                Thread.sleep(1000);
-                countAdd();
-                System.out.println(count);
-                transferMoney(fromAcct, toAcct, money);
-
-
+                Thread.sleep(new Random().nextInt(2000 - 1000) + 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            if (count < 30) {
 
+                transferMoney(fromAcct, toAcct, money);
+                countAdd();
 
+            } else {
+
+                System.out.println(count);
+                break;
+            }
         }
-
     }
 
-    private  void doTransfer(Account fromAcct, Account toAcct, int money) {
-        if (fromAcct.getMoney() == 0 || toAcct.getMoney() == 0) {
+    private void doTransfer(Account fromAcct, Account toAcct, int money) {
 
-        } else {
+        if (fromAcct.getMoney() > 0 && money > 0 && money <= fromAcct.getMoney()){
             fromAcct.debit(money);
             toAcct.credit(money);
+            System.out.println(this.getName());
+        } else{
 
         }
+
     }
 
-    public  void transferMoney(Account fromAcct, Account toAcct, int money) {
+    public void transferMoney(Account fromAcct, Account toAcct, int money) {
         String fromId = fromAcct.getID();
         String toId = toAcct.getID();
         if (fromId.compareTo(toId) < 0) {
@@ -102,8 +110,8 @@ class MyThread extends Thread {
 
     }
 
-    private void countAdd(){
-        synchronized (this){
+    private void countAdd() {
+        synchronized (this) {
             count++;
         }
     }
